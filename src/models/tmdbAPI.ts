1440d7bca34af93d82b371b0e1ae05650254e11f
Nicolas Vaquero <nicolas.vaquero@veepee.com>
2021-03-21 16:26:58 +0100

    Adds tmdb get resource by ID and credits

--- src/models/tmdbAPI.ts
+++ src/models/tmdbAPI.ts
@@ -1,4 +1,4 @@
-export type CommonProperties = {
+export type MultiSearchCommonProperties = {
   id?: number;
   media_type: string;
   name?: string;
@@ -6,19 +6,109 @@ export type CommonProperties = {
   adult?: boolean;
 };
 
-export type Movie = CommonProperties & {
+export type SearchMovie = MultiSearchCommonProperties & {
   media_type: 'movie';
   poster_path?: string | null;
   overview?: string;
 };
 
-export type TV = CommonProperties & {
+export type SearchTV = MultiSearchCommonProperties & {
   media_type: 'tv';
   poster_path?: string | null;
   overview?: string;
 };
 
-export type Person = CommonProperties & {
+export type SearchPerson = MultiSearchCommonProperties & {
   media_type: 'person';
   profile_path?: string | null;
+};
+
+type CreditMember = {
+  adult?: boolean;
+  gender?: number | null;
+  id?: number;
+  known_for_department?: string;
+  name?: string;
+  original_name?: string;
+  popularity?: number;
+  profile_path?: string | null;
+  credit_id?: string;
+};
+
+// TV Shows don't have cast_id
+type CastMovieTV = CreditMember & {
+  cast_id?: number;
+  character?: string;
+  order?: number;
+};
+
+type CrewMovieTV = CreditMember & {
+  department?: string;
+  job?: string;
+};
+
+type CreditPerson = {
+  id: number;
+  original_language: string;
+  episode_count: number;
+  overview: string;
+  origin_country: string[];
+  original_name: string;
+  vote_count: number;
+  name: string;
+  media_type: string;
+  popularity: number;
+  credit_id: string;
+  backdrop_path: string | null;
+  first_air_date: string;
+  vote_average: number;
+  genre_ids: number[];
+  poster_path: string | null;
+  original_title: string;
+  video: boolean;
+  title: string;
+  adult: boolean;
+  release_date: string;
+};
+
+type CastPerson = CreditPerson & {
+  character?: string;
+};
+
+type CrewPerson = CreditPerson & {
+  department: string;
+  job: string;
+};
+
+type Credits<Cast, Crew> = {
+  id?: number;
+  cast?: Cast[];
+  crew?: Crew[];
+};
+
+export type MovieCredits = Credits<CastMovieTV, CrewMovieTV>;
+export type TVCredits = Credits<CastMovieTV, CrewMovieTV>;
+export type PersonCredits = Credits<CastPerson, CrewPerson>;
+
+export type Detailed = {};
+
+// ToDo: add missing properties (resource details) to Movie and TV
+export type Movie = Detailed & SearchMovie;
+export type TV = Detailed & SearchTV;
+
+export type Person = Detailed & {
+  birthday?: string | null;
+  known_for_department?: string;
+  deathday?: string | null;
+  id?: number;
+  name?: string;
+  also_known_as?: string[];
+  gender?: number;
+  biography?: string;
+  popularity?: number;
+  place_of_birth?: string | null;
+  profile_path?: string | null;
+  adult?: boolean;
+  imdb_id?: string;
+  homepage?: string | null;
 };
--- src/services/tmdbAPI.ts
+++ src/services/tmdbAPI.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
-import { CommonProperties, Movie, Person, TV } from '../models/tmdbAPI';
+import {
+  Detailed,
+  Movie,
+  MovieCredits,
+  MultiSearchCommonProperties,
+  Person,
+  PersonCredits,
+  SearchMovie,
+  SearchPerson,
+  SearchTV,
+  TV,
+  TVCredits,
+} from '../models/tmdbAPI';
 
 const token = process.env.REACT_APP_TMDB_ACCESS_TOKEN;
 
@@ -11,7 +23,7 @@ tmdbAPI.defaults.headers.common = {
   Authorization: token ? `Bearer ${token}` : '',
 };
 
-type TMDBResponse<T extends CommonProperties> = {
+type TMDBResponse<T extends MultiSearchCommonProperties> = {
   page?: number;
   total_results?: number;
   total_pages?: number;
@@ -24,20 +36,52 @@ type MultiSearch = {
   include_adult?: boolean;
 };
 
-export const search = <T extends CommonProperties>(searchType: string) => ({
-  query,
-  page,
-  include_adult,
-}: MultiSearch): Promise<TMDBResponse<T>> =>
-  tmdbAPI.get(`search/${searchType}`, {
-    params: {
-      query,
-      page,
-      include_adult,
-    },
-  });
+export const getResource = <T, P>(
+  resource: string,
+  id?: number,
+  extraPath?: string
+) => async (params?: P): Promise<T> => {
+  try {
+    const _idPath = id ? `/${id}` : '';
+    const _extraPath = extraPath ? `/${extraPath}` : '';
+    const { data } = await tmdbAPI.get(`${resource}${_idPath}${_extraPath}`, {
+      params,
+    });
+    return data;
+  } catch (error) {
+    // ToDo: generate a pretty error to throw to the component
+    throw error;
+  }
+};
 
-export const multiSearch = search<Movie | TV | Person>('multi');
-export const movieSearch = search<Movie>('movie');
-export const tvSearch = search<TV>('tv');
-export const personSearch = search<Person>('person');
+const search = <T extends MultiSearchCommonProperties>(resource: string) =>
+  getResource<TMDBResponse<T>, MultiSearch>('search', undefined, resource);
+
+export const multiSearch = search<SearchMovie | SearchTV | SearchPerson>(
+  'multi'
+);
+export const movieSearch = search<SearchMovie>('movie');
+export const tvSearch = search<SearchTV>('tv');
+export const personSearch = search<SearchPerson>('person');
+
+const getResourceExtra = <T>(resource: string, extra?: string) => (
+  id: number
+) => getResource<T, null>(resource, id, extra)();
+
+const getResourceById = <T extends Detailed>(resource: string) => (
+  id: number
+) => getResourceExtra<T>(resource)(id);
+
+export const getMovieById = getResourceById<Movie>('movie');
+export const getTVById = getResourceById<TV>('tv');
+export const getPersonById = getResourceById<Person>('person');
+
+export const getMovieCredits = getResourceExtra<MovieCredits>(
+  'movie',
+  'credits'
+);
+export const getTVCredits = getResourceExtra<TVCredits>('tv', 'credits');
+export const getPersonCombinedCredits = getResourceExtra<PersonCredits>(
+  'person',
+  'combined_credits'
+);
